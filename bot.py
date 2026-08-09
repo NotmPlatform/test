3efc16e89d893e2148b5dcaff8dc7158871d83e7
NotmPlatform <48536035+NotmPlatform@users.noreply.github.com>
2026-08-09 13:04:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,7 +38,7 @@ if ADMIN_CHAT_ID_RAW:
 TRIBUTE_CM_URL = os.getenv("TRIBUTE_CM_URL", "https://t.me/tribute/app?startapp=sQSN")
 TRIBUTE_MARKETING_URL = os.getenv("TRIBUTE_MARKETING_URL", "https://t.me/tribute/app?startapp=sQVV")
 TRIBUTE_BD_URL = os.getenv("TRIBUTE_BD_URL", "https://t.me/tribute/app?startapp=sQWw")
-MANAGER_URL = os.getenv("MANAGER_URL", "https://t.me/BizManag")
+MANAGER_URL = os.getenv("MANAGER_URL", "https://t.me/forMRPL")
 DB_PATH = os.getenv("DB_PATH", "2026up_career_test.db")
 
 
